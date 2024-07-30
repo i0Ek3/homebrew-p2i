9f72ad170a5490ee92139f8a50174f7178d37763
i0Ek3 <kno30826@gmail.com>
2024-07-31 07:47:20 +0800

    dep: add url and sha256 item

--- p2i.rb
+++ p2i.rb
@@ -1,6 +1,8 @@
 class P2i < Formula
   desc "A Bash/Shell script to convert your PNG images into Mac OS specific type ICNS (iconset) !"
   homepage "https://github.com/i0Ek3/p2i"
+  url "https://github.com/i0Ek3/p2i/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "5af1198d23c3ca521a93883efb6a253a47e287c5b4ca9b41814f7cd32e3fc900"
   license "MIT"
 
   def install
